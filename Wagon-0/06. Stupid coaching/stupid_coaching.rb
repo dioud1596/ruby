2502cbbead1e4ee986d5c82edf4faa36387390ec
Edouard <edouard.rudolf@gmail.com>
2014-01-06 18:31:31 +0100

    first solution to ruby-0 challenge 6

--- Wagon-0/06. Stupid coaching/stupid_coaching.rb
+++ Wagon-0/06. Stupid coaching/stupid_coaching.rb
@@ -1,5 +1,14 @@
 def stupid_coaching 
-  # your code goes here
+	puts "what do you want to tell me ?"
+	answer = gets.chomp
+
+	if answer.include?"?"
+		puts "Silly question, get dressed and go to work !"
+	elsif answer == "I am going to work right now SIR !"
+	  	puts " I told you, you need to."
+	else
+		puts "I don't care son, get dressed and go to work !"
+	end
 end
 
 # Calling the method
