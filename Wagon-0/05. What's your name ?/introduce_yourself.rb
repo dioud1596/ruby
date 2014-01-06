5716ddb7f18cca8c5d5ca7e77ff192479f32b57a
Edouard <edouard.rudolf@gmail.com>
2014-01-06 17:37:04 +0100

    first solution to ruby-0 challenge 5

--- Wagon-0/05. What's your name ?/introduce_yourself.rb
+++ Wagon-0/05. What's your name ?/introduce_yourself.rb
@@ -1,1 +1,17 @@
-# your code goes here, introduce yourself !
+print "What is your first name ?"
+first_name = gets.chomp
+
+print "What is your middle name ?"
+middle_name = gets.chomp
+
+print " What is your last name ?"
+last_name = gets.chomp
+
+puts "Hello #{first_name} #{middle_name} #{last_name}".length
+
+puts "Hello"+" "+first_name+" "+middle_name+" "+last_name
+
+
+unless last_name.empty?s
+	puts "You are a good person"
+end
