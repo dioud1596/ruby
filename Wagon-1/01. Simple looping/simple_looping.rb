53e9c8879a8c80c612b4639f87129c457df7add8
Edouard <edouard.rudolf@gmail.com>
2014-01-07 10:48:43 +0100

    first solution to ruby-1 challenge 1

--- Wagon-1/01. Simple looping/simple_looping.rb
+++ Wagon-1/01. Simple looping/simple_looping.rb
@@ -1,12 +1,39 @@
 # The objective is to create a function that computes the sum of the integers from a min value to a max value
 
-def sum(min,max)
-  # your code here
+############
+# While loop
+############
+
+def sum(min, max)
+  sum = 0
+  counter = min 
+
+  while counter < max
+	sum += counter # sum = sum + counter
+	counter += 1 # counter = counter + 1
+  end
+  
+  sum
+
 end
+
+
+############
+# For loop
+############
+
+def sum(min, max)
+  sum = 0
+  for i in min..max
+    sum += i
+  end
+  sum
+end
+
+
 
 # Testing your code
 
 min = 1
 max = 100
-sum = sum(1,100)
-puts sum == 5050 # => true
+puts sum(min, max)
