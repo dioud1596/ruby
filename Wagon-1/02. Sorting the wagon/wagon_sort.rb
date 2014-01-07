16afe75544947b04baf3e55e6875a285fb1b211c
Edouard <edouard.rudolf@gmail.com>
2014-01-07 12:21:00 +0100

    first solution to ruby-1 challenge 2

--- Wagon-1/02. Sorting the wagon/wagon_sort.rb
+++ Wagon-1/02. Sorting the wagon/wagon_sort.rb
@@ -1,5 +1,21 @@
-def wagon_sort( students_array )
-  # your code to sort the array and output students
+def wagon_sort
+  array = []
+  puts "Type a student:"
+  student = gets.chomp
+  array << student
+  until student.empty?
+  	puts "Type another student:"
+  	student = gets.chomp
+  	array << student
+  end
+  	
+  puts "Congratulations ! Your Wagon has #{array.length} students"
+  array.sort.each do |mack|
+  	puts mack
+  end
+
 end
+
+wagon_sort
 
 # your code to interact with the user..
