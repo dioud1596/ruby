e5edfdc409f23717a7da3ba6bc78f29581eb0728
Edouard <edouard.rudolf@gmail.com>
2014-01-11 16:59:24 +0100

    ex 6

--- Wagon-0/06. Stupid coaching/stupid_coaching.rb
+++ Wagon-0/06. Stupid coaching/stupid_coaching.rb
@@ -3,42 +3,75 @@ def stupid_coaching()
 	puts "what do you want to say to your coach ?"
 	input = gets.chomp
 	
-	if input.include? "?"
-		puts "Silly question dude"
-	elsif input == "i am going to work"
-		puts "finally bastard"	
-	else
-		puts "I don't care i'm not ur mom"
-	end
-	
-end
-# Appel de la 
-puts stupid_coaching 
-
-
-def stupid_coaching2()
-	puts "please talk to your coach:"
-	input = gets.chomp
-
-	
-
-	if input == "I am going to work"
-		puts "good boy ! Take the subway, it will be faster"
-		puts" what do u want to say me again ?"
-		input2= gets.chomp
-
-		if input2.include? "?"
-			puts "Silly question, take the subway and go to work !"
-		elsif input2.upcase
-			puts "I can feel your motivation"
+	until input.include?("I am going to work")
+		if input.include?("?")
+		puts "Silly question dude"	
 		else
-			puts "I don't care son, take the subway and go to work !"
+		puts "I don't care i'm not ur mom"
 		end
+	puts "speak again"
+	input = gets.chomp
 	end
+	puts "Finally bastard"
 end
-puts stupid_coaching2
+
+# Appel de la fonction
+puts stupid_coaching
+
+a = 3
+
+b = a + 2
+
+
+def quelquechose(times)
+	return "quelquechose" * times
+end
+
+
+b = " j ai compris"
+
+a = quelquechose(4) + b
+
+puts a 
+
+puts(a == "quelquechosequelquechosequelquechosequelquechose j ai compris")
+
+def double_name(name)
+  result = name + " " + name
+	return result
+end
+
+bob = double_name("bob ")
+
+alice = double_name("alice ")
+
+
+puts bob
+puts alice
+
+
+# def stupid_coaching2()
+# 	puts "please talk to your coach:"
+# 	input = gets.chomp
+
+# 	if input == "I am going to work"
+# 		puts "good boy ! Take the subway, it will be faster"
+# 		puts" what do u want to say me again ?"
+# 		input2= gets.chomp
+
+# 		if input2.include? "?"
+# 			puts "Silly question, take the subway and go to work !"
+# 		elsif input2.upcase
+# 			puts "I can feel your motivation"
+# 		else
+# 			puts "I don't care son, take the subway and go to work !"
+# 		end
+# 	end
+
+# end
+
+# puts stupid_coaching2
 
 
 		
-
 
