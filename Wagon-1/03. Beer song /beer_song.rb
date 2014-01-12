ca20ed9d9d0d91196a2a901a1b23064b6b9dfe9f
Edouard <edouard.rudolf@gmail.com>
2014-01-12 17:22:38 +0100

    Exo 3

--- Wagon-1/03. Beer song /beer_song.rb
+++ Wagon-1/03. Beer song /beer_song.rb
@@ -1,41 +1,20 @@
-###Solution 1
 def beersong(nb_at_start)
-  case nb_at_start
-  when 0
-  	return
-  when 1
-  	puts "1 bottle of beer on the wall, 1 bottle of beer!"
-  	puts "Take one down, pass it around, no more bottles of beer on the wall!"
-  when 2
-  	puts "2 bottles of beer on the wall, 2 bottles of beer!"
-		puts "Take one down, pass it around, 1 bottle of beer on the wall!"
-  else
-  	puts "#{nb_at_start} bottles of beer on the wall, #{nb_at_start} bottles of beer!"
-  	puts "Take one down, pass it around, #{nb_at_start - 1} bottles of beer on the wall!"
+  until nb_at_start < 3
+    puts "#{nb_at_start} bottles of beer on the wall, #{nb_at_start} bottles of beer!"
+    puts "Take one down, pass it around,#{nb_at_start - 1} bottles of beer on the wall!"
+    nb_at_start -= 1
   end
-  beersong(nb_at_start - 1)
+ nb_at_start
+puts "2 bottles of beer on the wall, 2 bottles of beer!
+Take one down, pass it around, 1 bottle of beer on the wall!"
+puts "1 bottle of beer on the wall, 1 bottle of beer!
+Take one down, pass it around, no more bottles of beer on the wall!"
 end
+beersong(10)
 
-
-###Solution 2
-
-number_of_current_beers = 10
-while number_of_current_beers > 2
- 	puts "#{number_of_current_beers} bottles of beer on the wall, #{number_of_current_beers} bottles of beer!"
-  number_of_current_beers -= 1
-  puts "Take one down, pass it around, #{number_of_current_beers} bottles of beer on the wall!"
-end
-
-puts "2 bottles of beer on the wall, 2 bottles of beer!"
-puts "Take one down, pass it around, 1 bottle of beer on the wall!"
-
-puts "1 bottle of beer on the wall, 1 bottle of beer!"
-puts "Take one down, pass it around, no more bottles of beer on the wall!"
-
-beersong(ARGV[0].to_i)
+#beersong( ARGV[0] )
 
 # output (if called with 5 beers) :
-
 # => 5 bottles of beer on the wall, 5 bottles of beer!
 # => Take one down, pass it around, 4 bottles of beer on the wall!
 # => 4 bottles of beer on the wall, 4 bottles of beer!
