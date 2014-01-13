8ffb51e351174ef517176d8b28ab700f8c7eacad
Edouard <edouard.rudolf@gmail.com>
2014-01-13 09:19:25 +0100

    Exo 9

--- Wagon-1/09. Debug Gym/bugged_concatenate.rb
+++ Wagon-1/09. Debug Gym/bugged_concatenate.rb
@@ -1,17 +1,55 @@
 def bugged_concatenate(array)
+  # 2. fix the code: TypeError
   puts array.inject("") { |output, element| output + element.to_s }.upcase
 end
 
+
+# 1. fix the method call: ArgumentError
+bugged_concatenate([1, "9", 84, " ", "George Orwell"])
+
+
 def canard(array)
-	puts array.map {|x, y| x * 2}
-end
+  puts array.map {|x, y| x * 2}
+ end
 
-# Fix the method
-bugged_concatenate(["1", "9", "84", " ", "George Orwell"])
-
-# Duck Typing
 canard(["1", "9", "84"])
+
 canard([1, 9, 84])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
