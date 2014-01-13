977afa4ed149b9f471250a2c62b2f8d08e8f275f
Edouard <edouard.rudolf@gmail.com>
2014-01-13 10:54:54 +0100

    Exo 2

--- Wagon-1/02. Sorting the wagon/wagon_sort.rb
+++ Wagon-1/02. Sorting the wagon/wagon_sort.rb
@@ -1,5 +1,44 @@
 
 #V1
+#def get_students()
+   #students = []
+   #puts "Type a student"
+   #input = gets.chomp
+   #students << input
+  #while true
+    #puts "Type a student"
+    #input = gets.chomp
+    #students << input
+    #break if input.empty?  
+  #end
+  #students.reject! {|a| a.empty? }
+  #puts "congratulations you are #{students.length} in the wagon:"
+  #puts students.sort_by {|word| word.downcase}
+#end
+#get_students
+ 
+
+
+
+#V2
+#def get_students()
+  #array = []
+  #puts "Type a student"
+  #text = gets.chomp
+  #array << text
+ #until text.empty? 
+    #puts "type an other student"
+    #text = gets.chomp
+    #array << text
+  #end
+  #array.reject!{|x|x.empty?}
+  #puts " hello you are #{array.length} in this class"
+  #puts array.sort_by{|x|x.downcase}
+#end
+#get_students
+
+#V3
+#V1
 def get_students()
    students = []
    puts "Type a student"
@@ -8,36 +47,14 @@ def get_students()
   while true
     puts "Type a student"
     input = gets.chomp
+    break if input.empty?
     students << input
-    break if input.empty?  
   end
-  students.reject! {|a| a.empty? }
+  
   puts "congratulations you are #{students.length} in the wagon:"
   puts students.sort_by {|word| word.downcase}
 end
 get_students
- 
-
-
-
-#V2
-def get_students()
-  array = []
-  puts "Type a student"
-  text = gets.chomp
-  array << text
- until text.empty? 
-    puts "type an other student"
-    text = gets.chomp
-    array << text
-  end
-  array.reject!{|x|x.empty?}
-  puts " hello you are #{array.length} in this class"
-  puts array.sort_by{|x|x.downcase}
-end
-get_students
-
-
 
 
 
